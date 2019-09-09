30ffd511ae2757b1fa5fc8fcad2b66a0ffc516d4
Lucas Pereyra <lukas.gd7@gmail.com>
2019-09-09 00:22:03 -0300

    New migration user_id_to_articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,6 +2,7 @@ class Article < ApplicationRecord
     #La tabla => Articles
     #Campos
     #Metodos
+    belongs_to :user
     validates :title, presence: true, uniqueness: true
     validates :body, presence: true, length: { minimum: 20 }
 end
