ee06a6870c51e4bba2dbdd584f4193227447d21a
Lucas Pereyra <lukas.gd7@gmail.com>
2019-09-11 00:41:00 -0300

    Categories almost finished , one issue needs to be corrected

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
     before_action :set_categories
 
     private 
+
     def set_categories
         @categories = Category.all
     end
--- app/models/article.rb
+++ app/models/article.rb
@@ -4,11 +4,13 @@ class Article < ApplicationRecord
     #Metodos
     belongs_to :user
     has_many :comments
-
+    has_many :has_categories
+    has_many :categories, through: :has_categories
    
     validates :title, presence: true, uniqueness: true
     validates :body, presence: true, length: { minimum: 20 }
     before_save :set_visits_count
+    after_create :save_categories
 
     has_attached_file :cover, styles: { medium: "12380x720", thumb:"800x600" } 
     validates_attachment_content_type :cover, content_type: /\Aimage\/.*\Z/ 
