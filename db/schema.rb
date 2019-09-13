157c7a62aed242e137d71858d10d5d810abaf37c
Lucas Pereyra <lukas.gd7@gmail.com>
2019-09-13 00:22:10 -0300

    Add Concerns and update migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_11_021814) do
+ActiveRecord::Schema.define(version: 2019_09_13_030310) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -59,9 +59,9 @@ ActiveRecord::Schema.define(version: 2019_09_11_021814) do
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.string "name"
-    t.string "permission_level"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "permission_level", default: 1
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
