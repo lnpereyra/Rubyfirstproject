b03849ad09ff508b52f257f5ced935cfcb980f59
Lucas Pereyra <lukas.gd7@gmail.com>
2019-09-18 23:29:06 -0300

    Add helper and eraste alert notice

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+    def article_cover url,options = {}
+        html_class = options[:class]    
+        html_style = "background:url(#{url});width:100%,height:400px;background-size: cover;"
+        html = "<header class='#{html_class}' style='#{html_style}'> </header>"
+        
+    end
 end
